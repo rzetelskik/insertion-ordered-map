9a34750d349d5ceb37b0897359530ef07ddfb4c4
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-12-19 12:27:06 +0100

    tests

---
+++ tests.cc
@@ -0,0 +1,19 @@
+#include "insertion_ordered_map.h"
+
+#define BOOST_TEST_MODULE tests
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+struct cout_redirect {
+    cout_redirect(std::streambuf * new_buffer) : old( std::cout.rdbuf(new_buffer)) {}
+
+    ~cout_redirect() {
+        std::cout.rdbuf(old);
+    }
+
+private:
+    std::streambuf * old;
+};
+
+BOOST_AUTO_TEST_CASE(empty) {}
